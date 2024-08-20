ddfd2012ff50fa5ff870bb89b92417fab0576796
LONTSI YVES <56171587+lontsiyves@users.noreply.github.com>
2024-08-20 10:44:19 +0100

    Update Header.jsx

--- src/Components/Partial/Header.jsx
+++ src/Components/Partial/Header.jsx
@@ -64,7 +64,7 @@ export default function Header() {
             aria-expanded="false"
           >
             <span className="mr-2 d-none d-lg-inline text-gray-600 small">
-             Ennov
+             Lontsi
             </span>
             <img
               className="img-profile rounded-circle"
